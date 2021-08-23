438f9bb5df89a00f6ac6b9b83e0ad6921d48325f
Bruyok Vlad <djkill911@gmail.com>
2021-08-23 21:45:27 +0300

    Add to HEX convert

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 const bgButtonRandom = document.querySelector('.bgButtonRandom');
+const hexColor = document.querySelector('.hexColor');
 
-function randomBgColor (){
-    let x = Math.floor(Math.random() * 256);
-    let y = Math.floor(Math.random() * 256);
-    let z= Math.floor(Math.random() * 256);
-    let rgbRandom = "rgb(" + x + "," + y + "," + z + ")";
 
-    document.body.style.background = rgbRandom;
+function componentToHex(c) {
+    let hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
 }
 
+function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    return
+}
+
+function randomBgColor () {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+
+
+    rgbToHex(x, y, z);
+
+    let rgbRandom = "rgb(" + x + "," + y + "," + z + ")";
+    document.body.style.background = rgbRandom;
+    let textHexColor = 'HEX color:';
+    hexColor.innerHTML = rgbToHex(x, y, z);
+}
 bgButtonRandom.addEventListener("click", randomBgColor);
